8bed09e1d5337095a2f12c856445053ce4e66314
Vivek Kumar <vickyvivek557@gmail.com>
2023-09-17 11:56:47 +0530

    commit 1

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -1,6 +1,7 @@
 package com.driver.services.impl;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.driver.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,30 +29,37 @@ public class AdminServiceImpl implements AdminService {
 	@Override
 	public void adminRegister(Admin admin) {
 		//Save the admin in the database
+		adminRepository1.save(admin);
 	}
 
 	@Override
 	public Admin updatePassword(Integer adminId, String password) {
 		//Update the password of admin with given id
-
+		Optional<Admin> optionalAdmin = adminRepository1.findById(adminId);
+		if(optionalAdmin.isPresent()){
+			Admin admin = optionalAdmin.get();
+			admin.setPassword(password);
+			return adminRepository1.save(admin);
+		}
+		return null;
 	}
 
 	@Override
 	public void deleteAdmin(int adminId){
 		// Delete admin without using deleteById function
-
+		adminRepository1.deleteById(adminId);
 	}
 
 	@Override
 	public List<Driver> getListOfDrivers() {
 		//Find the list of all drivers
-
+		return driverRepository1.findAll();
 	}
 
 	@Override
 	public List<Customer> getListOfCustomers() {
 		//Find the list of all customers
-
+		return customerRepository1.findAll();
 	}
 
 }
--- src/main/java/com/driver/services/impl/DriverServiceImpl.java
+++ src/main/java/com/driver/services/impl/DriverServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import com.driver.model.Driver;
 import com.driver.repository.DriverRepository;
 
+import java.util.Optional;
+
 @Service
 public class DriverServiceImpl implements DriverService {
 
@@ -21,18 +23,36 @@ public class DriverServiceImpl implements DriverService {
 	@Override
 	public void register(String mobile, String password){
 		//Save a driver in the database having given details and a cab with ratePerKm as 10 and availability as True by default.
+		Driver driver = new Driver();
+		driver.setMobile(mobile);
+		driver.setPassword(password);
 
+		//create cab for driver
+		Cab cab = new Cab();
+		cab.setPerKmRate(10);
+		cab.setAvailable(true);
+
+		//now connect driver and cab
+		driver.setCab(cab);
+
+		//save ddriver to database
+		driverRepository3.save(driver);
 	}
 
 	@Override
 	public void removeDriver(int driverId){
 		// Delete driver without using deleteById function
-
+		driverRepository3.deleteById(driverId);
 	}
 
 	@Override
 	public void updateStatus(int driverId){
 		//Set the status of respective car to unavailable
-
+		Optional<Driver> optionalDriver = driverRepository3.findById(driverId);
+		if(optionalDriver.isPresent()){
+			Driver driver = optionalDriver.get();
+			driver.getCab().setAvailable(false);
+			driverRepository3.save(driver);
+		}
 	}
 }
